ddef4485d61a4ad561e5fd315615c325c80507c2
ssramko <samuel.sramko@tatum.io>
2022-02-16 08:43:44 +0100

    Final INTENT addresses + token distribution - mainnet deployment address

--- scripts/deploy_upgradable.js
+++ scripts/deploy_upgradable.js
@@ -13,8 +13,8 @@ async function main () {
   ]);
   await tent.deployed();
   console.log(tent.deployTransaction.creates);
-  await tent.renounceRole('0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6', '0x80d8bac9a6901698b3749fe336bbd1385c1f98f2');
-  await tent.renounceRole('0x65d7a28e3265b37a6474929f336521b332c1681b933f6cb9f3376673440d862a', '0x80d8bac9a6901698b3749fe336bbd1385c1f98f2');
+  await tent.renounceRole('0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6', '0xCF9e127455D28e7362380AEc1b92DdEE8200b295');
+  await tent.renounceRole('0x65d7a28e3265b37a6474929f336521b332c1681b933f6cb9f3376673440d862a', '0xCF9e127455D28e7362380AEc1b92DdEE8200b295');
   // const goldax = await upgrades.deployProxy(Goldax, [
   //   'GOLDAX', 'GLDT',
   // ]);
